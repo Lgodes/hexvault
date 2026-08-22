fa03659ec0894f3f28cf6f61b5283a926d6c7909
Lgodes <AlbadlCruz98@gmail.com>
2026-08-22 02:46:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='hexvault-v180';
+const VERSION='hexvault-v182';
 const STATIC=[
   '/', '/index.html', '/manifest.webmanifest',
   '/logo4.png','/logo4-hd-transparent.png','/coin-epic.png','/d20-epic.png','/assets/jsQR.min.js',
